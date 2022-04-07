6a44bf11043cffd1eb416d304aa6f5833ddeeffb
Allan <allan.tao.qi@gmail.com>
2022-04-06 22:46:25 -0500

    added sorting dropdown (content adjusts automatically without page reload)

--- djackets_vue/src/views/Category.vue
+++ djackets_vue/src/views/Category.vue
@@ -3,9 +3,40 @@
     <div class="columns is-multiline">
       <div class="column is-12">
         <h2 class="is-size-2 has-text-centered">{{ category.name }}</h2>
+        
+        <div class="dropdown is-hoverable">
+          <div class="dropdown-trigger">
+            <button class="button" aria-haspopup="true" aria-controls="dropdown-menu">
+              <span id="sortLabel">{{ this.sortType }}</span>
+              <span class="icon is-small">
+                <i class="fas fa-angle-down" aria-hidden="true"></i>
+              </span>
+            </button>
+          </div>
+          <div class="dropdown-menu" id="dropdown-menu" role="menu">
+            <div class="dropdown-content">
+              <a class="dropdown-item" @click="setSortType(0)">
+                Sort by price (ascending)
+              </a>
+              <a class="dropdown-item" @click="setSortType(1)">
+                Sort by price (descending)
+              </a>
+              <a class="dropdown-item" @click="setSortType(2)">
+                Sort by stock (ascending)
+              </a>
+              <a class="dropdown-item" @click="setSortType(3)">
+                Sort by stock (descending)
+              </a>
+            </div>
+          </div>
+        </div>
       </div>
 
-      <ProductBox v-for="product in sortByStockAsc" v-bind:key="product.id" v-bind:product="product" />
+      <ProductBox v-if="this.sortType === 'Sort by price (ascending)'" v-for="product in sortByPriceAsc" v-bind:key="product.id" v-bind:product="product" />
+      <ProductBox v-if="this.sortType === 'Sort by price (descending)'" v-for="product in sortByPriceDesc" v-bind:key="product.id" v-bind:product="product" />
+      <ProductBox v-if="this.sortType === 'Sort by stock (ascending)'" v-for="product in sortByStockAsc" v-bind:key="product.id" v-bind:product="product" />
+      <ProductBox v-if="this.sortType === 'Sort by stock (descending)'" v-for="product in sortByStockDesc" v-bind:key="product.id" v-bind:product="product" />
+
     </div>
   </div>
 </template>
@@ -23,7 +54,8 @@
       return {
         category: {
           products: []
-        }
+        },
+        sortType: 'Sort by price (ascending)'
       }
     },
 
@@ -46,7 +78,7 @@
     computed: {
       /* SORTING
       The order (asc/desc) is reversed between the name and params due to the order that
-      the <ProductBox> element is populated. Go by name for correct order.
+      the <ProductBox> element is populated. Go by function name for correct order.
       */
       sortByPriceAsc: function () {
         return _.orderBy(this.category.products, 'price', 'desc')
@@ -91,6 +123,25 @@
           })
 
         this.$store.commit('setIsLoading', false)
+      },
+
+      setSortType(a) {
+        if(a === 0)
+        {
+          this.sortType = 'Sort by price (ascending)'
+        }
+        else if(a === 1)
+        {
+          this.sortType = 'Sort by price (descending)'
+        }
+        else if(a === 2)
+        {
+          this.sortType = 'Sort by stock (ascending)'
+        }
+        else if(a === 3)
+        {
+          this.sortType = 'Sort by stock (descending)'
+        }
       }
     }
   }
