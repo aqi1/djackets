1a2a846017662e29ad901003cb43189cc51ed8ed
Allan <allan.tao.qi@gmail.com>
2022-04-18 23:12:21 -0500

    Added Saul's navbar

--- djackets_vue/src/App.vue
+++ djackets_vue/src/App.vue
@@ -1,5 +1,7 @@
 <template>
+
   <div id="wrapper">
+  		<!--
     <nav class="navbar is-dark">
 	  <div class="navbar-brand">
 	    <router-link to="/" class="navbar-item"><strong>Black Dog Apparel</strong></router-link>
@@ -9,8 +11,7 @@
 		  <span aria-hidden="true"></span>
 		  <span aria-hidden="true"></span>
 		</a>
-      </div>
-	  
+  </div>
 	  <div class="navbar-menu" id="navbar-menu" v-bind:class="{'is-active': showMobileMenu }">
 	  	<div class="navbar-start">
 	  		<div class="navbar-item">
@@ -54,7 +55,79 @@
 		</div>
 	  </div>
 	</nav>
+	-->
 	
+  <section class="hero is-primary is-small">
+    <!-- Hero head: will stick at the top -->
+    <div class="hero-head">
+      <nav class="navbar">
+        <div class="container">
+          <div class="navbar-brand">
+            <router-link to="/" class="navbar-item"><strong>BLACK DOG APPAREL</strong></router-link>
+            <a role="button" class="navbar-burger" aria-label="menu" aria-expanded="false" data-target="navbarMenuHeroA" @click="showMobileMenu = !showMobileMenu">
+              <span aria-hidden="true"></span>
+              <span aria-hidden="true"></span>
+              <span aria-hidden="true"></span>
+            </a>
+          </div>
+          <div id="navbarMenuHeroA" class="navbar-menu" v-bind:class="{'is-active': showMobileMenu}">
+            <div class="navbar-start">
+              <div class="navbar-item">
+                <form method="get" action="/search">
+                  <div class="field has-addons">
+                    <div class="control is-expanded">
+                      <input type="text" class="input" placeholder="Search" name="query">
+                    </div>
+                    <div class="control">
+                      <button class="button is-success">
+                        <span class="icon fas fa-search">
+                        </span>
+                      </button>
+                    </div>
+                  </div>
+                </form>
+              </div>
+            </div>
+
+            <div class="navbar-end">
+              <div class="navbar-item">
+                <div class="buttons">
+                  <router-link to="/tops" class="button is-primary is-rounded is-inverted">Tops</router-link>
+                  <router-link to="/bottoms" class="button is-primary is-rounded is-inverted">Bottoms</router-link>
+                  <template v-if="$store.state.isAuthenticated">
+                    <router-link to="/myaccount" class="button is-primary is-success">My Account</router-link>
+                  </template>
+                  <template v-else>
+                    <router-link to="/login" class="button is-primary is-success">Log In</router-link>
+                  </template>
+
+                  <router-link to="/cart" class="button is-success">
+                    <span class="icon"><i class="fas fa-shopping-cart"></i></span>
+                    <span>({{ cartTotalLength }}) Item(s)</span>
+                  </router-link>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </nav>
+    </div>
+
+    <!-- Hero content: will be in the middle -->
+    <!--
+    <div class="hero-body">
+      <div class="container has-text-centered">
+        <p class="title">
+          WELCOME TO BLACK DOG APPAREL
+        </p>
+        <p class="subtitle">
+          BUY OUR EPIC FAKE SHIRTS
+        </p>
+      </div>
+    </div>
+  	-->
+  </section>
+
 	<div class="is-loading-bar has-text-centered" v-bind:class="{'is-loading': $store.state.isLoading }">
 		<div class="lds-dual-ring"></div>
 	</div>
