f14941629c2234c08ccfb5a3e3a6fbcb5dbf8811
Allan <allan.tao.qi@gmail.com>
2022-04-20 20:56:15 -0500

    Fixed RGBA thumbnail bug
    - Fixed issue where if the product image was a .png with transparency, conversion into .jpg would throw an exception

--- djackets_django/product/models.py
+++ djackets_django/product/models.py
@@ -55,12 +55,11 @@ class Product(models.Model):
         return ''
 
   def make_thumbnail(self, image, size=(300,200)):
-    img = Image.open(image)
-    img.convert('RGB')
+    img = Image.open(image).convert('RGB')
     img.thumbnail(size)
 
     thumb_io = BytesIO()
-    img.save(thumb_io, 'JPEG', quality=85)
+    img.save(thumb_io, 'JPEG', quality=90)
 
     thumbnail = File(thumb_io, name=image.name)
 
